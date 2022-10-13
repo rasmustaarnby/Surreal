f5ac1b352a73fd037360b9e3426e38d37796dd31
Italo <DarkGhostHunter@Gmail.com>
2022-10-13 01:26:30 -0300

    Adds column listing procedure.

--- src/Schema/SurrealSchemaBuilder.php
+++ src/Schema/SurrealSchemaBuilder.php
@@ -4,7 +4,23 @@ namespace Laragear\Surreal\Schema;
 
 use Illuminate\Database\Schema\Builder;
 
+/**
+ * @property-read \Laragear\Surreal\SurrealConnection $connection
+ */
 class SurrealSchemaBuilder extends Builder
 {
+    /**
+     * Get the column listing for a given table.
+     *
+     * @param  string  $table
+     * @return array
+     */
+    public function getColumnListing($table)
+    {
+        $table = $this->connection->getTablePrefix().$table;
 
+        $results = $this->connection->statement($this->grammar->compileColumnListing($table));
+
+        return $this->connection->getPostProcessor()->processColumnListing($results);
+    }
 }
