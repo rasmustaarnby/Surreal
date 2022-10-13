ce1cd8b1f4e255e7f15c60069169e7147fb0f7f4
Italo <DarkGhostHunter@Gmail.com>
2022-10-13 01:26:49 -0300

    Creates function for column listing statement.

--- src/Schema/SurrealSchemaGrammar.php
+++ src/Schema/SurrealSchemaGrammar.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Schema\Grammars\Grammar;
 
 class SurrealSchemaGrammar extends Grammar
 {
-
+    /**
+     * Compile the query to determine the list of columns.
+     *
+     * @param  string  $table
+     * @return string
+     */
+    public function compileColumnListing(string $table): string
+    {
+        return "INFO FOR TABLE $table";
+    }
 }
