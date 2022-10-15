4dc686d7c4e9d24e846c344ba845c8b3c0fd2f37
Italo <DarkGhostHunter@Gmail.com>
2022-10-15 01:03:11 -0300

    Moves Mock Connection to self-bootable trait.

---
+++ tests/AssertsMockConnection.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Support\Collection;
+use Laragear\Surreal\Contracts\SurrealClient;
+use Laragear\Surreal\JsonRpc\ClientMessage;
+use Mockery\MockInterface;
+
+trait AssertsMockConnection
+{
+    /** @var \Laragear\Surreal\Contracts\SurrealClient|\Mockery\MockInterface */
+    protected $client;
+
+    /** @var \Laragear\Surreal\SurrealConnection|\Mockery\MockInterface */
+    protected $surreal;
+
+    protected function setUpAssertsMockConnection(): void
+    {
+        $this->client = $this->mock(SurrealClient::class, static function (MockInterface $mock): void {
+            $mock->expects('configure')->with([
+                'driver' => 'ws',
+                'ns' => 'forge',
+                'db' => 'forge',
+                'username' => 'forge',
+                'password' => 'forge',
+                'database' => 'rpc',
+                'host' => 'localhost',
+                'port' => 8000
+            ]);
+            $mock->expects('start')->andReturnNull();
+        });
+
+        $this->surreal = $this->getConnection('surreal');
+    }
+
+    protected function expectsMessage(string $statement, array $bindings = [], $return = new Collection())
+    {
+        $this->client->expects('send')->withArgs(function (ClientMessage $query) use ($statement, $bindings): bool {
+            static::assertSame('query', $query->method);
+            static::assertSame($statement, $query->params[0]->statement);
+            static::assertSame($bindings, $query->params[1]->parameters);
+
+            return true;
+        })->andReturn($return);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,23 @@ namespace Tests;
 
 use Laragear\Surreal\SurrealServiceProvider;
 use Orchestra\Testbench\TestCase as BaseTestCase;
+use function class_uses;
 
 abstract class TestCase extends BaseTestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        foreach (class_uses($this) as $trait) {
+            $method = 'setUp'.class_basename($trait);
+
+            if (method_exists($this, $method)) {
+                $this->{$method}();
+            }
+        }
+    }
+
     protected function getPackageProviders($app): array
     {
         return [SurrealServiceProvider::class];
