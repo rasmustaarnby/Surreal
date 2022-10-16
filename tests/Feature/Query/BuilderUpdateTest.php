dc636c9c0e444695c74048ddcdd075faf8e7ea4f
Italo <DarkGhostHunter@Gmail.com>
2022-10-16 00:55:33 -0300

    Completes tests for UPDATE.

--- tests/Feature/Query/BuilderUpdateTest.php
+++ tests/Feature/Query/BuilderUpdateTest.php
@@ -2,9 +2,40 @@
 
 namespace Tests\Feature\Query;
 
+use RuntimeException;
+use Tests\AssertsMockConnection;
 use Tests\TestCase;
 
 class BuilderUpdateTest extends TestCase
 {
+    use AssertsMockConnection;
 
+    public function test_update_operation(): void
+    {
+        $this->expectsMessage('UPDATE user:1 SET fred = $?', ['thud']);
+
+        $this->surreal->update('UPDATE user:1 SET fred = $?', ['thud']);
+    }
+
+    public function test_updates_through_query(): void
+    {
+        $this->expectsMessage('UPDATE `user` SET `fred` = $? WHERE `foo` = $?', ['thud', 'bar']);
+
+        $this->surreal->table('user')->where('foo', 'bar')->update(['fred' => 'thud']);
+    }
+
+    public function test_updates_through_query_using_id(): void
+    {
+        $this->expectsMessage('UPDATE "user:1" SET `fred` = $? WHERE `foo` = $?', ['thud', 'bar']);
+
+        $this->surreal->id('user:1')->where('foo', 'bar')->update(['fred' => 'thud']);
+    }
+
+    public function test_updates_with_join_throws_error(): void
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('SurrealDB does not support JOIN operations. Use FETCH or <-/-> instead.');
+
+        $this->surreal->id('user:1')->join('foo', 'bar')->update([]);
+    }
 }
