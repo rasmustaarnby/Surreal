e2883c16af30f3ad16ef058217dc9086fdcf70cf
Italo <DarkGhostHunter@Gmail.com>
2022-10-16 00:55:14 -0300

    Adds tests for DELETE.

---
+++ tests/Feature/Query/BuilderDeleteTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Feature\Query;
+
+use RuntimeException;
+use Tests\AssertsMockConnection;
+use Tests\TestCase;
+
+class BuilderDeleteTest extends TestCase
+{
+    use AssertsMockConnection;
+
+    public function test_delete(): void
+    {
+        $this->expectsMessage('DELETE user');
+
+        $this->surreal->delete('DELETE user');
+    }
+
+    public function test_delete_with_bindings(): void
+    {
+        $this->expectsMessage('DELETE user WHERE foo = $?', ['foo' => 'bar']);
+
+        $this->surreal->delete('DELETE user WHERE foo = $?', ['foo' => 'bar']);
+    }
+
+    public function test_query_delete(): void
+    {
+        $this->expectsMessage('DELETE FROM `user`');
+
+        $this->surreal->table('user')->delete();
+    }
+
+    public function test_query_delete_with_bindings(): void
+    {
+        $this->expectsMessage('DELETE FROM `user` WHERE `foo` = $?', ['bar']);
+
+        $this->surreal->table('user')->where('foo', 'bar')->delete();
+    }
+
+    public function test_query_delete_id_directly(): void
+    {
+        $this->expectsMessage('DELETE "user:id"');
+
+        $this->surreal->id('user:id')->delete();
+    }
+
+    public function test_query_delete_id_with_join_throws_exception(): void
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('SurrealDB does not support JOIN operations. Use FETCH or <-/-> instead.');
+
+        $this->surreal->id('user:id')->join('foo', 'bar')->delete();
+    }
+}
