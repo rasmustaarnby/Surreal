4b3b48e9998c0d79f8f1a20f78dafcb8daa4f8c2
Italo <DarkGhostHunter@Gmail.com>
2022-10-17 00:46:40 -0300

    Adds Builder stubs for autocompletion.

---
+++ .stubs/Builder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Illuminate\Database\Query {
+
+    /**
+     * @method $this return(\Laragear\Surreal\Query\ReturnType|string|array ...$type)
+     * @method $this returnNone()
+     * @method $this timeout(\DateInterval|\Carbon\CarbonInterval|int $timeout)
+     * @method $this parallel()
+     * @method \Illuminate\Support\Collection|null create(array $attributes = [])
+     */
+    class Builder
+    {
+        //
+    }
+}
